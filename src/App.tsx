1ce7b9ffdd7afef3e4d9450f7695f3bb988c0d50
ChauNgocMinh <chaungocminh202@gmail.com>
2024-08-04 23:31:31 +0700

    update

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import { Button } from '@mui/material'
+import './App.scss'
+import TextField from '@mui/material/TextField'
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+        <TextField
+          id=""
+          label=""
+        />
+        <Button>xnxx</Button>
+      </h1>
+    </>
+  )
+}
+
+export default App
